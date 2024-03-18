6e1f91d1305a2187c4aff2d24e56a0491455fe92
wany0011 <wanyong_sin@hotmail.com>
2024-03-18 12:27:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import  Flask,request,render_template
+from flask import Flask, request, render_template
 import google.generativeai as palm
 
-
-palm.configure(api_key="AIzaSyCaNG0OeUwcDAls6_aaAdFdSAujquF4vIo")
-model = { 'model': "models/chat-bison-001"}
+palm.configure(api_key="AIzaSyCCT1K99BJ1JbLwhCE7qOcQ5KOZcPJ9ZZ4")
+model = {
+    "model": "models/chat-bison-001",
+}
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
-    return (render_template("index.html"))
+    return(render_template("index.html"))
 
-@app.route("/palm",methods=["GET","POST"])
+
+@app.route("/palm", methods=["GET","POST"])
 def palm_flask():
     q = request.form.get("q")
     print(q)
@@ -19,7 +21,8 @@ def palm_flask():
         **model,
         messages=q
     )
-    return (render_template("reply.html",r=r.last))
+    print(r.last)
+    return(render_template("reply.html",r=r.last))
 
 if __name__ == "__main__":
     app.run()
